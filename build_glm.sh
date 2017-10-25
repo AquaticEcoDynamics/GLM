5921133b368729ca0c4141fc2bf1fdb6fde72f25
Casper <casper.boon@uwa.edu.au>
2017-10-25 08:39:33 +0800

    merge

--- build_glm.sh
+++ build_glm.sh
@@ -2,50 +2,61 @@
 
 if [ "$GLM_CONFIGURED" != "true" ] ; then
   . ./GLM_CONFIG
-
-  export OSTYPE=`uname -s`
-
-  if [ "$FORTRAN_COMPILER" = "IFORT" ] ; then
-    if [ -d /opt/intel/bin ] ; then
-      . /opt/intel/bin/compilervars.sh intel64
-    fi
-    which ifort >& /dev/null
-    if [ $? != 0 ] ; then
-      echo ifort compiler requested, but not found
-      exit 1
-    fi
-  fi
-
-  if [ "$FORTRAN_COMPILER" = "IFORT" ] ; then
-    export PATH="/opt/intel/bin:$PATH"
-    export FC=ifort
-    export NETCDFHOME=/opt/intel
-  else
-    export FC=gfortran
-    export NETCDFHOME=/usr
-    if [ "$OSTYPE" == "Darwin" ] ; then
-      export NETCDFHOME=/opt/local
-    fi
-  fi
-
-  export F77=$FC
-  export F90=$FC
-  export F95=$FC
-
-  export MPI=OPENMPI
-
-  export NETCDFINC=$NETCDFHOME/include
-  export NETCDFINCL=${NETCDFINC}
-  export NETCDFLIBDIR=$NETCDFHOME/lib
-  export NETCDFLIB=${NETCDFLIBDIR}
-  export NETCDFLIBNAME="-lnetcdff -lnetcdf"
-else
-  export CURDIR=`pwd`
 fi
 
-if [ 1 = 0 ] ; then
+export OSTYPE=`uname -s`
+
+if [ "$FORTRAN_COMPILER" = "IFORT" ] ; then
+  if [ -d /opt/intel/bin ] ; then
+    . /opt/intel/bin/compilervars.sh intel64
+  fi
+  which ifort >& /dev/null
+  if [ $? != 0 ] ; then
+    echo ifort compiler requested, but not found
+    exit 1
+  fi
+fi
+
+if [ "$FORTRAN_COMPILER" = "IFORT" ] ; then
+  export PATH="/opt/intel/bin:$PATH"
+  export FC=ifort
+  export NETCDFHOME=/opt/intel
+else
+  export FC=gfortran
+  export NETCDFHOME=/usr
+  if [ "$OSTYPE" == "Darwin" ] ; then
+    export NETCDFHOME=/opt/local
+  fi
+fi
+
+export F77=$FC
+export F90=$FC
+export F95=$FC
+
+export MPI=OPENMPI
+
+export NETCDFINC=$NETCDFHOME/include
+export NETCDFINCL=${NETCDFINC}
+export NETCDFLIBDIR=$NETCDFHOME/lib
+export NETCDFLIB=${NETCDFLIBDIR}
+export NETCDFLIBNAME="-lnetcdff -lnetcdf"
+
+if [ "$AED2DIR" = "" ] ; then
+  export AED2DIR=../libaed2
+fi
+if [ "$PLOTDIR" = "" ] ; then
+  export PLOTDIR=../libplot
+fi
+if [ "$UTILDIR" = "" ] ; then
+  export UTILDIR=../libutil
+fi
+
 if [ "$FABM" = "true" ] ; then
-  export COMPILATION_MODE=production
+  if [ "$DEBUG" = "true" ] ; then
+    export COMPILATION_MODE=debug
+  else
+    export COMPILATION_MODE=production
+  fi
 
   if [ ! -d $FABMDIR ] ; then
     echo "FABM directory not found"
@@ -90,48 +101,10 @@ fi
 cd ${UTILDIR}
 make || exit 1
 
-fi
-
 if [ -f ${CURDIR}/src/glm ] ; then
   /bin/rm ${CURDIR}/src/glm
 fi
 cd ${CURDIR}
 make || exit 1
-
-cd ${CURDIR}
-
-if [ "$OSTYPE" = "Linux" ] ; then
-  VERSION=`grep GLM_VERSION src/glm.h | cut -f2 -d\"`
-  echo glm version $VERSION
-  if [ -f /etc/debian_version ] ; then
-    VERSDEB=`head -1 debian/changelog | cut -f2 -d\( | cut -f1 -d-`
-    echo debian version $VERSDEB
-    if [ "$VERSION" != "$VERSDEB" ] ; then
-      echo updating debian version
-      dch --newversion ${VERSION}-0 "new version ${VERSION}"
-    fi
-
-    fakeroot make -f debian/rules binary || exit 1
-  fi
-
-  cd ${CURDIR}/win
-  ${CURDIR}/vers.sh $VERSION
-  cd ${CURDIR}/win-dll
-  ${CURDIR}/vers.sh $VERSION
-
-  cd ${CURDIR}
-  if [ ! -d bin/ubuntu/$(lsb_release -rs) ] ; then
-    mkdir -p bin/ubuntu/$(lsb_release -rs)/
-  fi
-  mv ../glm*.deb bin/ubuntu/$(lsb_release -rs)/
-fi
-if [ "$OSTYPE" = "Darwin" ] ; then
-  if [ ! -d ${CURDIR}/bin/macos ] ; then
-     mkdir -p ${CURDIR}/bin/macos
-  fi
-  cd ${CURDIR}/macos
-  /bin/bash macpkg.sh ${HOMEBREW}
-  mv ${CURDIR}/macos/glm_*.zip ${CURDIR}/bin/macos/
-fi
 
 exit 0
