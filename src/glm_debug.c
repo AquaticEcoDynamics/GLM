716feabbb721b860a0839036f53c71f36130f6df
Casper Boon <casper.boon@uwa>
2024-03-22 12:56:50 +0800

    bump copyright notes

--- src/glm_debug.c
+++ src/glm_debug.c
@@ -9,7 +9,7 @@
  *                                                                            *
  *     http://aquatic.science.uwa.edu.au/                                     *
  *                                                                            *
- * Copyright 2020 - 2023 -  The University of Western Australia               *
+ * Copyright 2020 - 2024 -  The University of Western Australia               *
  *                                                                            *
  *  This file is part of GLM (General Lake Model)                             *
  *                                                                            *
--- src/glm_types.F90
+++ src/glm_types.F90
@@ -11,7 +11,7 @@
 !#                                                                             #
 !#     http://aquatic.science.uwa.edu.au/                                      #
 !#                                                                             #
-!# Copyright 2013 - 2023 -  The University of Western Australia                #
+!# Copyright 2013 - 2024 -  The University of Western Australia                #
 !#                                                                             #
 !#  This file is part of GLM (General Lake Model)                              #
 !#                                                                             #
